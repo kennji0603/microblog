37f5528bfeec26525b26f597ca85964f89bfa765
kennji0603 <tetsuya.matsu.tm@gmail.com>
2022-08-14 15:46:43 +0900

    投稿機能実装

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from http.client import HTTPResponse
+from .models import Post
 
 def frontpage(request):
-  return render(request, "blog/frontpage.html")
+    posts = Post.objects.all()
+    return render(request, "blog/frontpage.html", {"posts": posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, "blog/post_detail.html", {"post": post})
